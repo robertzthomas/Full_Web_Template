3072ebaa4ebc7b1a2375015174448de9d29f3a69
robertzthomas <46911581+robertzthomas@users.noreply.github.com>
2022-04-29 16:50:09 -0700

    init
    ALPHA initial commit. . no functionality

---
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void destroy() {
+    }
+}
